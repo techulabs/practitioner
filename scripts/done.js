a6a09312f43c7eec4244b6c0e95a28a4ad1f2bae
Angel Manuel Rayo <angelmanuel.rayo.contractor@bbva.com>
2020-09-28 21:49:06 +0200

    First version

---
+++ scripts/done.js
@@ -0,0 +1,7 @@
+var done = document.getElementById("done")
+if (done)
+    done.href="/practitioner";
+
+var back = document.getElementById("arrow-back");
+    if (back)
+        back.href="/practitioner";
